89d957502754da538d96a45e53a069985690ea6f
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-01-09 15:40:09 +0000

    Refactor print_reversed_list_integer function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-"""
-This module contains a function to print a list of integers in reverse order.
-"""
-
-
-def print_reversed_list_integer(my_list):
-    """
-    Prints a list of integers in reverse order.
-
-    Args:
-        my_list (list): The list of integers.
-
-    Returns:
-        None
-    """
-    my_list.reverse()
-    for i, num in enumerate(my_list):
-        print(f'{num:d}')
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print('{:d}'.format(my_list[i]))
