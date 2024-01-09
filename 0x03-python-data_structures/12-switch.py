d81928fa782c2247e555863162bbefaeb6526039
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-01-09 15:40:46 +0000

    Swap the values of two variables in Python

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-This module demonstrates swapping the values of two variables.
-"""
-
-A = 89
-B = 10
-A, B = B, A
-print(f"a={A} - b={B}")
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
